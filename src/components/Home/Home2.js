a6ff26541a79ebdca56c0557d758308b71a344b4
Hiba7898 <hibakandaoui083@gmail.com>
2024-09-20 16:01:51 +0100

    2009

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/prgrm.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -19,42 +19,41 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple">Hiba Kandaoui , </b>
+              </i>
+              a full stack web developer , competent in front-end and back-end technologies,
+              <br />
+              <br />
+              creation of creative sites and web applications.  &nbsp;
+              <i>
+                <b className="purple">Welcome to my world of coding 💻 </b> and 
+                creativity! 🎨
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              Proficient in languages such
+               as <b className="purple">JavaScript, Php, Python </b> 
+               I specialize in both 
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  front-end and back-end
                 </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+                 
+              </i> development .
+              &nbsp; developing powerful server-side applications,
+              or working with frameworks like
+              <b className="purple">
+                  {" "}
+                  Laravel .
+                </b> 
             </p>
           </Col>
+
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={myImg} style={{borderRadius: "200px"}} className="img-fluid" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,9 @@ function Type() {
     <Typewriter
       options={{
         strings: [
+          "Full Stack Developer | Integrator",
           "Software Developer",
           "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
         ],
         autoStart: true,
         loop: true,
