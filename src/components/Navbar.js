7e209a5670d62257f3aa5dd2ae889ab35540c8df
Hiba7898 <hibakandaoui083@gmail.com>
2024-09-24 15:46:24 +0100

    24/09/24

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
+import logo from "../Assets/logo-remove.png";
 import { Link } from "react-router-dom";
 import { AiOutlineMail } from "react-icons/ai"; // Icône de l'email
 import {
@@ -42,6 +42,7 @@ function NavBar() {
         <Navbar.Brand href="/" className="d-flex">
           <img src={logo} className="img-fluid logo" alt="brand" />
         </Navbar.Brand>
+        
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => {
