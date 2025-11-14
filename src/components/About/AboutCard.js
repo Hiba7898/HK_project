ea8b5ee9afe9c0d307c9a96ba1ae9dbef8fd5227
Hiba7898 <hibakandaoui083@gmail.com>
2025-11-14 21:47:09 +0100

    14/11/2025

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,3 +1,43 @@
+// import React from "react";
+// import Card from "react-bootstrap/Card";
+// import { useTranslation } from 'react-i18next';
+
+
+// function AboutCard() {
+//   const { t } = useTranslation();
+//   return (
+//     <Card className="quote-card-view">
+//       <Card.Body>
+//         <blockquote className="blockquote mb-0">
+//           <p style={{ textAlign: "justify" }}>
+//             {t('AboutCard1')}<span className="purple">{t('AboutCard2')}</span>
+//             {t('AboutDesc1')} 
+//             <br />
+//             <br />
+//             {t('AboutDesc2')}
+//             {t('AboutDesc3')}  
+//             <br />
+//             <br />
+//             {t('AboutJourney1')}
+//             {t('AboutJourney2')} 💻🚀
+//             <br />
+//            {/*  <br />
+//             {t('AboutJourney3')}
+//             {t('AboutCode1')}
+//             <br /> */}
+//             <br />
+//             {t('AboutCode2')}
+//           </p>
+//           <footer className="blockquote-footer">HK</footer>
+//         </blockquote>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
+// export default AboutCard;
+
+
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { useTranslation } from 'react-i18next';
@@ -9,7 +49,7 @@ function AboutCard() {
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
+          <p className="about-card-text">
             {t('AboutCard1')}<span className="purple">{t('AboutCard2')}</span>
             {t('AboutDesc1')} 
             <br />
